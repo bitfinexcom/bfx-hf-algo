07f07a94bb2f5a1a07090016b3a9da04c1097714
Cris Mihalache <me@f3rno.com>
2018-11-12 16:12:31 +0100

    (fix) add startDB/connectDB to example

--- examples/ao_host.js
+++ examples/ao_host.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 process.env.DEBUG = '*,-bfx:api:ws:on_channel_message'
 
 const debug = require('debug')('bfx:ao:examples:ao-host')
+const { startDB, connectDB } = require('bfx-hf-models')
 const AOHost = require('./ao_host')
 const Iceberg = require('./iceberg')
 const TWAP = require('./twap')
@@ -14,84 +15,95 @@ const {
   WS_URL, REST_URL, API_KEY, API_SECRET
 } = process.env
 
-const host = new AOHost({
-  aos: [Iceberg, TWAP, AccumulateDistribute],
-  apiKey: API_KEY,
-  apiSecret: API_SECRET,
-  wsURL: WS_URL,
-  restURL: REST_URL
-})
+const run = async () => {
+  await startDB(`${__dirname}/../db`)
+  await connectDB('hf-algo')
 
-host.on('ao:start', (instance) => {
-  const { state = {} } = instance
-  const { id, gid } = state
-  debug('started AO %s [gid %s]', id, gid)
-})
-
-host.on('ao:stop', (instance) => {
-  const { state = {} } = instance
-  const { id, gid } = state
-  debug('stopped AO %s [gid %s]', id, gid)
-})
-
-host.on('ws2:auth:error', (packet) => {
-  debug('error authenticating: %j', packet)
-})
-
-host.on('error', (err) => {
-  debug('error: %s', err)
-})
-
-host.once('ws2:auth:success', async () => {
-  const gid = await host.startAO('bfx.accumulate_distribute', {
-    symbol: 'tBTCUSD',
-    amount: -0.2,
-    sliceAmount: -0.1,
-    sliceInterval: 10000,
-    intervalDistortion: 0.20,
-    amountDistortion: 0.20,
-    orderType: 'RELATIVE', // MARKET, LIMIT, RELATIVE
-    relativeOffset: { type: 'ask', args: [20], delta: -10 },
-    relativeCap: { type: 'bid', delta: 10 },
-    submitDelay: 150,
-    cancelDelay: 150,
-    catchUp: true, // if true & behind, ignore slice interval (after prev fill)
-    awaitFill: true,
-    _margin: false
+  const host = new AOHost({
+    aos: [Iceberg, TWAP, AccumulateDistribute],
+    apiKey: API_KEY,
+    apiSecret: API_SECRET,
+    wsURL: WS_URL,
+    restURL: REST_URL
   })
 
-  debug('started AO %s', gid)
-
-  /*
-  const gid = await host.startAO('bfx.twap', {
-    symbol: 'tBTCUSD',
-    amount: -0.5,
-    sliceAmount: -0.1,
-    sliceInterval: 10,
-    priceDelta: 100,
-    priceTarget: 16650,
-    priceCondition: TWAP.Config.PRICE_COND.MATCH_LAST,
-    tradeBeyondEnd: false,
-    orderType: 'LIMIT',
-    submitDelay: 150,
-    cancelDelay: 150,
-    _margin: false
+  host.on('ao:start', (instance) => {
+    const { state = {} } = instance
+    const { id, gid } = state
+    debug('started AO %s [gid %s]', id, gid)
   })
-  */
 
-  /*
-  const gid = host.startAO('bfx.iceberg', {
-    symbol: 'tBTCUSD',
-    price: 21000,
-    amount: -0.5,
-    sliceAmount: -0.1,
-    excessAsHidden: true,
-    orderType: 'LIMIT',
-    submitDelay: 150,
-    cancelDelay: 150,
-    _margin: false,
+  host.on('ao:stop', (instance) => {
+    const { state = {} } = instance
+    const { id, gid } = state
+    debug('stopped AO %s [gid %s]', id, gid)
   })
-  */
-})
 
-host.connect()
+  host.on('ws2:auth:error', (packet) => {
+    debug('error authenticating: %j', packet)
+  })
+
+  host.on('error', (err) => {
+    debug('error: %s', err)
+  })
+
+  host.once('ws2:auth:success', async () => {
+    const gid = await host.startAO('bfx.accumulate_distribute', {
+      symbol: 'tBTCUSD',
+      amount: -0.2,
+      sliceAmount: -0.1,
+      sliceInterval: 10000,
+      intervalDistortion: 0.20,
+      amountDistortion: 0.20,
+      orderType: 'RELATIVE', // MARKET, LIMIT, RELATIVE
+      relativeOffset: { type: 'ask', args: [20], delta: -10 },
+      relativeCap: { type: 'bid', delta: 10 },
+      submitDelay: 150,
+      cancelDelay: 150,
+      catchUp: true, // if true & behind, ignore slice interval (after prev fill)
+      awaitFill: true,
+      _margin: false
+    })
+
+    debug('started AO %s', gid)
+
+    /*
+    const gid = await host.startAO('bfx.twap', {
+      symbol: 'tBTCUSD',
+      amount: -0.5,
+      sliceAmount: -0.1,
+      sliceInterval: 10,
+      priceDelta: 100,
+      priceTarget: 16650,
+      priceCondition: TWAP.Config.PRICE_COND.MATCH_LAST,
+      tradeBeyondEnd: false,
+      orderType: 'LIMIT',
+      submitDelay: 150,
+      cancelDelay: 150,
+      _margin: false
+    })
+    */
+
+    /*
+    const gid = host.startAO('bfx.iceberg', {
+      symbol: 'tBTCUSD',
+      price: 21000,
+      amount: -0.5,
+      sliceAmount: -0.1,
+      excessAsHidden: true,
+      orderType: 'LIMIT',
+      submitDelay: 150,
+      cancelDelay: 150,
+      _margin: false,
+    })
+    */
+  })
+
+  host.connect()
+}
+
+try {
+  run()
+} catch (e) {
+  debug('error: %s', e.message)
+}
