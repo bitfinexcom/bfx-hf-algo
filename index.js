950437c801ddbb6402c98cf221907314c994d3ba
Cris Mihalache <me@f3rno.com>
2018-11-22 12:31:02 +0200

    (refactor) rename exports Iceberg/TWAP

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   AOHost: require('./lib/ao_host'),
-  IcebergOrder: require('./lib/iceberg'),
-  TWAPOrder: require('./lib/twap'),
+  Iceberg: require('./lib/iceberg'),
+  TWAP: require('./lib/twap'),
   AccumulateDistribute: require('./lib/accumulate_distribute'),
   PingPong: require('./lib/ping_pong'),
   NoDataError: require('./lib/errors/no_data')
