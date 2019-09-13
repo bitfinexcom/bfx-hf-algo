271fd48c35530f0538f76557fd694267308ead58
Cris Mihalache <me@f3rno.com>
2019-09-14 03:13:20 +0700

    (standard --fix)

--- test/iceberg/events/life_start.js
+++ test/iceberg/events/life_start.js
@@ -7,13 +7,15 @@ const onLifeStart = require('iceberg/events/life_start')
 
 describe('iceberg:events:life_start', () => {
   it('submits orders on startup', (done) => {
-    onLifeStart({ h: {
-      emitSelf: (eName) => {
-        return new Promise((resolve) => {
-          assert.strictEqual(eName, 'submit_orders')
-          resolve()
-        }).then(done).catch(done)
+    onLifeStart({
+      h: {
+        emitSelf: (eName) => {
+          return new Promise((resolve) => {
+            assert.strictEqual(eName, 'submit_orders')
+            resolve()
+          }).then(done).catch(done)
+        }
       }
-    } })
+    })
   })
 })
