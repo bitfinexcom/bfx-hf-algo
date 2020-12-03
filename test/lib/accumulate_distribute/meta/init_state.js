29831c86aa818c6f65acffd87a3ff60b20509c41
alexstotsky <alexander.stotsky@code-care.pro>
2020-12-03 13:56:08 +0200

    test-accum-dist: upd init_state tests

--- test/lib/accumulate_distribute/meta/init_state.js
+++ test/lib/accumulate_distribute/meta/init_state.js
@@ -18,6 +18,17 @@ describe('accumulate_distribute:meta:init_state', () => {
     stubOrderAmountsGen.restore()
   })
 
+  it('initializes order amounts, floats', () => {
+    const stubOrderAmountsGen = sinon.stub(orderAmounts, 'gen').returns([0.001, 0.002])
+    const state = initState({ amount: 0.003 })
+
+    assert.deepStrictEqual(state.orderAmounts, [0.001, 0.002])
+    assert.deepStrictEqual(state.args, { amount: 0.003 })
+    assert.strictEqual(state.remainingAmount, 0.003)
+
+    stubOrderAmountsGen.restore()
+  })
+
   it('throws an error if gen_order_amounts returns a greater total amount than requested by the user', () => {
     const stubOrderAmountsGen = sinon.stub(orderAmounts, 'gen').returns([1, 2, 1])
 
