0dcc10c3a79f4fcdd8f55005600c452ad0143e8a
alexstotsky <alexander.stotsky@code-care.pro>
2020-12-10 18:00:10 +0200

    is_target_met: update tests

--- test/lib/twap/util/is_target_met.js
+++ test/lib/twap/util/is_target_met.js
@@ -10,4 +10,10 @@ describe('twap:util:is_target_met', () => {
     assert.ok(isTargetMet({ priceTarget: 1, priceDelta: 0.2 }, 1.2))
     assert.ok(!isTargetMet({ priceTarget: 1, priceDelta: 0.2 }, 1.3))
   })
+
+  it('verifies that the price target is met, floats', () => {
+    assert.ok(isTargetMet({ priceTarget: 2.4, priceDelta: 0 }, 2.4))
+    assert.ok(isTargetMet({ priceTarget: 2.4, priceDelta: 2.3 }, 4.7))
+    assert.ok(!isTargetMet({ priceTarget: 2.4, priceDelta: 2.3 }, 4.8))
+  })
 })
