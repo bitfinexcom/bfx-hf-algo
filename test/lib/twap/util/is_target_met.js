e2e0378735b0ccf53377c7978495f57a9cf18f53
Abhishek Shrestha <avsek.stha07@gmail.com>
2021-02-17 18:48:15 +0545

    tests for twap float issue fix

--- test/lib/twap/util/is_target_met.js
+++ test/lib/twap/util/is_target_met.js
@@ -9,5 +9,6 @@ describe('twap:util:is_target_met', () => {
     assert.ok(isTargetMet({ priceTarget: 1, priceDelta: 0 }, 1))
     assert.ok(isTargetMet({ priceTarget: 1, priceDelta: 0.2 }, 1.2))
     assert.ok(!isTargetMet({ priceTarget: 1, priceDelta: 0.2 }, 1.3))
+    assert.ok(isTargetMet({ priceTarget: 5.6, priceDelta: 5.8 }, 11.4))
   })
 })
