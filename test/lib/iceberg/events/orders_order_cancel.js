55e18360fb631e9605ba8798b39a1f40d0a5ca23
Abhishek Shrestha <avsek.stha07@gmail.com>
2021-03-03 13:06:28 +0545

    cancels all orders set by iceberg algo on life stop event

--- test/lib/iceberg/events/orders_order_cancel.js
+++ test/lib/iceberg/events/orders_order_cancel.js
@@ -6,31 +6,12 @@ const onOrderCancel = require('../../../../lib/iceberg/events/orders_order_cance
 
 describe('iceberg:events:orders_order_cancel', () => {
   it('submits all known orders for cancellation & stops operation', (done) => {
-    let call = 0
-    const orderState = {
-      1: 'some_order_object'
-    }
-
     onOrderCancel({
-      state: {
-        gid: 100,
-        orders: orderState
-      },
-
       h: {
         debug: () => {},
-        emit: async (eName, gid, orders) => {
-          if (call === 0) {
-            assert.strictEqual(gid, 100)
-            assert.strictEqual(eName, 'exec:order:cancel:all')
-            assert.deepStrictEqual(orders, orderState)
-            call += 1
-          } else if (call === 1) {
-            assert.strictEqual(eName, 'exec:stop')
-            done()
-          } else {
-            done(new Error('too many events emitted'))
-          }
+        emit: async (eName) => {
+          assert.strictEqual(eName, 'exec:stop')
+          done()
         }
       }
     })
