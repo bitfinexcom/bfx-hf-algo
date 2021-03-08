6538f389e0c09a469d26eec0a567152f5823d5bf
Robert Kowalski <rok@kowalski.gd>
2021-03-08 16:40:19 +0100

    test: remove unnecessary test

--- test/lib/twap/meta/init_state.js
+++ test/lib/twap/meta/init_state.js
@@ -15,9 +15,4 @@ describe('twap:meta:init_state', () => {
     const state = initState(args)
     assert.deepStrictEqual(state.args, args)
   })
-
-  it('seeds null interval', () => {
-    const state = initState({ amount: 42 })
-    assert.strictEqual(state.interval, null)
-  })
 })
