64dc7f503b3344b7f0e2aee5e55dc80ceab52222
Abhishek Shrestha <avsek.stha07@gmail.com>
2021-05-04 20:03:07 +0545

    tests added with data for golden and death cross

--- test/lib/ma_crossover/events/data_managed_candles.js
+++ test/lib/ma_crossover/events/data_managed_candles.js
@@ -3,13 +3,38 @@
 
 const assert = require('assert')
 const sinon = require('sinon')
-const { EMA } = require('bfx-hf-indicators')
+const { EMA, SMA } = require('bfx-hf-indicators')
 const { Candle } = require('bfx-api-node-models')
 const dataManagedCandles = require('../../../../lib/ma_crossover/events/data_managed_candles')
 
 const candleOpts = { open: 1, high: 1.2, low: 0.9, close: 1, volume: 100, mts: 100 }
 const CANDLE = new Candle(candleOpts)
 const CANDLE_KEY = 'key:1m:tTESTBTC:TESTUSD'
+
+const UPTREND_CANDLE_INFO = [
+  { open: 2994.06, high: 3028.50, low: 2988, close: 3020.64 },
+  { open: 3020.64, high: 3036, low: 3008.52, close: 3030.73 },
+  { open: 2994.06, high: 3028.50, low: 2988, close: 3020.64 },
+  { open: 3030.75, high: 3059.40, low: 3020.46, close: 3054.33 },
+  { open: 2994.06, high: 3028.50, low: 2988, close: 3020.64 },
+  { open: 3054.34, high: 3060, low: 3036.53, close: 3052 },
+  { open: 3051.99, high: 3109.87, low: 3049.32, close: 3102.29 },
+  { open: 3102.29, high: 3104.99, low: 3080.81, close: 3088.70 },
+  { open: 3088.71, high: 3147, low: 3081.50, close: 3146.95 },
+  { open: 3146.95, high: 3204.04, low: 3132.11, close: 3184.07 },
+  { open: 3184.06, high: 3190, low: 3141.12, close: 3165.73 },
+  { open: 3165.73, high: 3168.55, low: 3125.39, close: 3148.81 },
+  { open: 3148.74, high: 3178.86, low: 3148.58, close: 3151.95 },
+  { open: 3151.95, high: 3162.82, low: 3111.89, close: 3144.59 },
+  { open: 3144.58, high: 3150, low: 3085.06, close: 3107.02 }
+]
+
+const DOWNTREND_CANDLE_INFO = [
+  ...UPTREND_CANDLE_INFO,
+  { open: 3107.01, high: 3178.93, low: 3104, close: 3161.56 },
+  { open: 3161.56, high: 3914.15, low: 3147.49, close: 3163.66 },
+  { open: 3163.65, high: 3269, low: 3150.89, close: 3254.83 }
+]
 
 const getInstance = ({
   params = {}, argParams = {}, stateParams = {}, helperParams = {}
@@ -171,5 +196,65 @@ describe('ma_crossover:events:data_managed_candles', () => {
     assert.ok(!sawExecStop, 'should not have stopped the algo')
 
     stubbedCrossed.restore()
+  })
+
+  it('submits order when golden cross is achieved', async () => {
+    let sawSubmitOrder = false
+    let sawExecStop = false
+
+    const i = getInstance({
+      stateParams: {
+        longIndicator: new SMA([9]),
+        shortIndicator: new SMA([3])
+      },
+      helperParams: {
+        emitSelf: async (eventName) => {
+          if (eventName === 'submit_order') {
+            sawSubmitOrder = true
+          }
+        },
+
+        emit: async (eventName) => {
+          if (eventName === 'exec:stop') {
+            sawExecStop = true
+          }
+        }
+      }
+    })
+
+    const uptrendCandles = UPTREND_CANDLE_INFO.map(c => { return new Candle(c) })
+    await dataManagedCandles(i, uptrendCandles, { chanFilter: { key: CANDLE_KEY } })
+    assert.ok(sawSubmitOrder, 'should have submitted order')
+    assert.ok(sawExecStop, 'should have stopped the algo')
+  })
+
+  it('submits order when death cross is achieved', async () => {
+    let sawSubmitOrder = false
+    let sawExecStop = false
+
+    const i = getInstance({
+      stateParams: {
+        longIndicator: new SMA([9]),
+        shortIndicator: new SMA([3])
+      },
+      helperParams: {
+        emitSelf: async (eventName) => {
+          if (eventName === 'submit_order') {
+            sawSubmitOrder = true
+          }
+        },
+
+        emit: async (eventName) => {
+          if (eventName === 'exec:stop') {
+            sawExecStop = true
+          }
+        }
+      }
+    })
+
+    const downtrendCandles = DOWNTREND_CANDLE_INFO.map(c => { return new Candle(c) })
+    await dataManagedCandles(i, downtrendCandles, { chanFilter: { key: CANDLE_KEY } })
+    assert.ok(sawSubmitOrder, 'should have submitted order')
+    assert.ok(sawExecStop, 'should have stopped the algo')
   })
 })
