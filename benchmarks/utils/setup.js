e716b37a02655cc967a4e61982d2814a4e93ed68
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-05-17 21:10:11 +0300

    setup clinic & autocannon for benchmark testing

---
+++ benchmarks/utils/setup.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const restify = require('restify')
+
+function setupBenchmark(execute) {
+  const server = restify.createServer()
+
+  server.get('/', async (req, res, next) => {
+    res.send(await execute() || null)
+    next()
+  })
+
+  server.listen(3000)
+
+  process.on('SIGINT', function () {
+    console.error('Caught SIGINT, shutting down.')
+    server.close()
+  })
+}
+
+module.exports = {
+  setupBenchmark
+}
