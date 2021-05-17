527d1e73d65398e67426c68bcdcec48fa64b572e
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-05-17 21:11:23 +0300

    benchmark host connect/destroy

---
+++ benchmarks/basic.js
@@ -0,0 +1,9 @@
+const { setupBenchmark } = require('./utils/setup')
+const { spawnHost, connectHost, destroyHost } = require('./fixtures/host')
+
+setupBenchmark(async () => {
+  const host = spawnHost()
+
+  await connectHost(host)
+  await destroyHost(host)
+})
---
+++ benchmarks/fixtures/host.js
@@ -0,0 +1,67 @@
+
+const {
+  AOHost, PingPong, Iceberg, TWAP, AccumulateDistribute, MACrossover
+} = require('../..')
+
+const { API_KEY, API_SECRET } = process.env
+
+if (!API_KEY || !API_SECRET) throw new Error('API_KEY and API_SECRET are required')
+
+function spawnHost () {
+  const wsSettings = {
+    apiKey: API_KEY,
+    apiSecret: API_SECRET,
+    dms: 4
+  }
+  const host = new AOHost({
+    aos: [PingPong, Iceberg, TWAP, AccumulateDistribute, MACrossover],
+    wsSettings
+  })
+
+  host.on('ao:start', (instance) => {
+    const { state = {} } = instance
+    const { id, gid } = state
+    console.log('started AO %s [gid %s]', id, gid)
+  })
+
+  host.on('ao:stop', (instance) => {
+    const { state = {} } = instance
+    const { id, gid } = state
+    console.log('stopped AO %s [gid %s]', id, gid)
+  })
+
+  host.on('auth:error', (packet) => {
+    console.log('error authenticating: %j', packet)
+  })
+
+  host.on('error', (err) => {
+    console.log('error: %s', err)
+  })
+
+  return host
+}
+
+function connectHost(host) {
+  return new Promise((res, rej) => {
+    host.connect()
+
+    host.once('error', (err) => {
+      rej(err)
+    })
+    host.once('ready', async () => {
+      res()
+    })
+  })
+}
+
+function destroyHost(host) {
+  host.removeAllListeners()
+  host.close()
+  host.cleanState()
+}
+
+module.exports = {
+  spawnHost,
+  connectHost,
+  destroyHost
+}
