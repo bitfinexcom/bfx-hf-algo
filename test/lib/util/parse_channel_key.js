88efb8ec34cbff316b40d4e472e2f0a590bd49db
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2021-05-26 20:21:28 -0300

    handle funding keys

--- test/lib/util/parse_channel_key.js
+++ test/lib/util/parse_channel_key.js
@@ -5,21 +5,49 @@ const { expect } = require('chai')
 const parseChannelKey = require('../../../lib/util/parse_channel_key')
 
 describe('parseChannelKey', () => {
-  it('should parse channel key', () => {
-    const details = parseChannelKey('key:1m:LEOUSD')
-    expect(details).to.eql({
-      key: 'key',
-      tf: '1m',
-      symbol: 'LEOUSD'
+  describe('trading candles key', () => {
+    it('should parse channel key', () => {
+      const details = parseChannelKey('trade:1m:LEOUSD')
+      expect(details).to.eql({
+        type: 'trade',
+        tf: '1m',
+        symbol: 'LEOUSD'
+      })
+    })
+
+    it('should be able to parse symbols with colons', () => {
+      const details = parseChannelKey('trade:1m:tTESTBTC:TESTUSD')
+      expect(details).to.eql({
+        type: 'trade',
+        tf: '1m',
+        symbol: 'tTESTBTC:TESTUSD'
+      })
     })
   })
 
-  it('should be able to parse symbols with colons', () => {
-    const details = parseChannelKey('key:1m:tTESTBTC:TESTUSD')
-    expect(details).to.eql({
-      key: 'key',
-      tf: '1m',
-      symbol: 'tTESTBTC:TESTUSD'
+  describe('funding candles key', () => {
+    it('should parse channel key', () => {
+      const details = parseChannelKey('trade:1m:LEOUSD:a30:p2:p30')
+      expect(details).to.eql({
+        type: 'trade',
+        tf: '1m',
+        symbol: 'LEOUSD',
+        aggr: 'a30',
+        end: 'p30',
+        start: 'p2'
+      })
+    })
+
+    it('should be able to parse symbols with colons', () => {
+      const details = parseChannelKey('trade:1m:tTESTBTC:TESTUSD:a30:p2:p30')
+      expect(details).to.eql({
+        type: 'trade',
+        tf: '1m',
+        symbol: 'tTESTBTC:TESTUSD',
+        aggr: 'a30',
+        end: 'p30',
+        start: 'p2'
+      })
     })
   })
 })
