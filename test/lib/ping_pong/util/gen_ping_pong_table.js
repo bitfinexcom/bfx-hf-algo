6c59b1c78d165ee1d3d49ced00df2634a63be925
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-06-10 13:24:05 +0300

    rename methods

--- test/lib/ping_pong/util/gen_ping_pong_table.js
+++ test/lib/ping_pong/util/gen_ping_pong_table.js
@@ -3,7 +3,7 @@
 
 const assert = require('assert')
 const genPingPongTable = require('../../../../lib/ping_pong/util/gen_ping_pong_table')
-const { getPings, getPongs, extract } = require('../../../../lib/ping_pong/util/ping_pong_table')
+const { getKeys, getValues, extract } = require('../../../../lib/ping_pong/util/ping_pong_table')
 
 const args = {
   pingPrice: 0,
@@ -19,9 +19,9 @@ const args = {
 describe('ping_pong:util:gen_ping_pong_table', () => {
   it('generates ping pong table with correct prices', () => {
     const pingPongTable = genPingPongTable(args)
-    assert.deepStrictEqual(getPings(pingPongTable).length, args.orderCount, 'invalid ping pong table')
-    assert.deepStrictEqual(getPings(pingPongTable), ['5.6000', '7.5667', '9.5333', '11.500'], 'does not generate correct ping prices')
-    assert.deepStrictEqual(getPongs(pingPongTable), ['6.4000', '8.3667', '10.333', '12.300'], 'does not generate correct pong prices')
+    assert.deepStrictEqual(getKeys(pingPongTable).length, args.orderCount, 'invalid ping pong table')
+    assert.deepStrictEqual(getKeys(pingPongTable), ['5.6000', '7.5667', '9.5333', '11.500'], 'does not generate correct ping prices')
+    assert.deepStrictEqual(getValues(pingPongTable), ['6.4000', '8.3667', '10.333', '12.300'], 'does not generate correct pong prices')
   })
 
   it('generates the ping and pong price same as user input when orderCount is 1', () => {
@@ -33,7 +33,7 @@ describe('ping_pong:util:gen_ping_pong_table', () => {
     }
     const pingPongTable = genPingPongTable(singleOrderCountArgs)
     const [price] = extract(pingPongTable, '50000')
-    assert.deepStrictEqual(getPings(pingPongTable).length, 1, 'invalid ping pong table')
+    assert.deepStrictEqual(getKeys(pingPongTable).length, 1, 'invalid ping pong table')
     assert.deepStrictEqual(price, '50500', 'does not generate correct ping and pong prices')
   })
 })
