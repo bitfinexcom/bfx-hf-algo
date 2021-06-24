6d7b02d39d7cb67d813c4bf44e2b020347d1d231
Abhishek Shrestha <avsek.stha07@gmail.com>
2021-06-24 19:18:52 +0545

    Acc dist issue for catch up fixes (#166)
    * slice interval for catch up option enabled typo fix
    * wait for timeout delay first before emitting the interval tick
    * tests updated with timeout
    * cancel all orders by gid
    * test update
    * using timeout as a util function

--- test/lib/accumulate_distribute/events/life_start.js
+++ test/lib/accumulate_distribute/events/life_start.js
@@ -4,6 +4,7 @@
 const assert = require('assert')
 const { EMA } = require('bfx-hf-indicators')
 const lifeStart = require('../../../../lib/accumulate_distribute/events/life_start')
+const timeout = require('../../../util/timeout')
 
 const getInstance = ({
   params = {}, argParams = {}, stateParams = {}, helperParams = {}
@@ -18,8 +19,10 @@ const getInstance = ({
   },
 
   h: {
+    timeout,
     debug: () => {},
     emit: async () => {},
+    emitSelf: async () => {},
     updateState: async () => {},
     scheduleTick: async () => {},
     notifyUI: async () => {},
