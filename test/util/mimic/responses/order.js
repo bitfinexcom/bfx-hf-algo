3cadfef670eb3eff5b6922d3c0643c67a7bfcbf2
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-06-25 14:42:11 +0300

    order response mock

---
+++ test/util/mimic/responses/order.js
@@ -0,0 +1,40 @@
+module.exports = function ({ id, gid, cid, amount, status }) {
+  return [
+    id,
+    gid,
+    cid,
+    'tAAABBB',
+    1624612619335,
+    1624612619337,
+    amount,
+    5,
+    'EXCHANGE LIMIT',
+    null,
+    null,
+    null,
+    0,
+    status, // ACTIVE | CANCELED | EXECUTED
+    null,
+    null,
+    5,
+    0,
+    0,
+    0,
+    null,
+    null,
+    null,
+    0,
+    0,
+    null,
+    null,
+    null,
+    'API>BFX',
+    null,
+    null,
+    {
+      _HF: 1,
+      label: 'OCOCO | 5 @ 5  | triggers 5 @ 5 (stop 5)',
+      aff_code: 'xZvWHMNR'
+    }
+  ]
+}
