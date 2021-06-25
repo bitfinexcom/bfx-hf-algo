bb0e3540b834cc895aaa8077b1f18115103beb08
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-06-25 14:45:51 +0300

    test OCOCO: submits OCO order after initial order is filled

--- test/integration/ococo.js
+++ test/integration/ococo.js
@@ -6,7 +6,7 @@ const { delay } = require('bluebird')
 const { expect } = require('chai')
 
 const { spawnHost, connectHost, destroyHost } = require('../util/host')
-const { NEW_ORDER } = require('../util/mimic/signal_types')
+const { NEW_ORDER, ORDER_CANCEL } = require('../util/mimic/signal_types')
 const ApiSpy = require('../util/mimic/testing/api_spy')
 const BaseApiMock = require('../util/base_api_mock')
 const orderResponse = require('../util/mimic/responses/order')
@@ -64,4 +64,25 @@ describe('OCOCO', () => {
       })
   })
 
+  it('submits OCO order', async () => {
+    await performOrder(host)
+    await delay(1_000)
+
+    const spyConn = spyServer.connections[0]
+    const spySession = apiMock.getSessions()[0]
+    const [{ cid, gid }] = spyConn.findReceived(NEW_ORDER).map(message => message.fields[1])
+
+    spySession.streamToMainChannel(ORDER_CANCEL, orderResponse({ id: 0, gid, cid, amount: 0, status: 'EXECUTED' }))
+    await delay(1_000)
+
+    expect(spyConn.countReceived(NEW_ORDER)).to.eq(2)
+    spyConn
+      .received(NEW_ORDER, ({ fields: [placeholder, details] }, i) => {
+        if (i === 1) {
+          expect(details.price).to.eq('1.7000')
+          expect(details.price_oco_stop).to.eq('2.1000')
+          expect(details.cid).to.be.a('number')
+        }
+      })
+  })
 })
